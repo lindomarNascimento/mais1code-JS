3dc5595019a4f1c14aa6fa68947e8b2954d8f05c
lindomar <linndomarnascimento17@gmail.com>
2023-06-07 20:39:45 -0300

    js if else

---
+++ index.js
@@ -0,0 +1,53 @@
+// >
+// < 
+// >=
+// <=
+// ==      = apenas o valor
+// ===     = comparar tipo e o valor
+// !=      = apenas o valor
+// !==        = comparar tipo e o valor
+
+// verdadeiro e falso = verdedeiro
+// falso e verdadeiro = verdadeiro
+
+// var anoNascimento = window.prompt('Qual seu ano de nascimento') // 1998
+
+// var resultado = 2023 - anoNascimento
+
+// console.log(resultado)
+
+// if (resultado >= 18) {
+//   console.log('Sim o usuario é maior de idade')
+// } else {
+//   console.log('Não pode acessar')
+// }
+
+// var idade = window.prompt('Digite sua idade')
+// var nota = window.prompt('Digite a nota')
+
+// tenha mais de 18 anos e tenha uma nota maior que 6
+
+// var resposta = idade >= 18 && nota >= 6
+
+// !not
+// console.log(!resposta)
+
+// exercicio 1
+//  ajude o professor, ele digitara 3 notas , calcule a media e retorne 
+//  se ele passou ou não de ano  , não enviar em console.log
+// deixa BUNITO estiliza bastante.
+
+// exercicio 2
+// 1 = pedra
+// 2 = papel
+// 3 = tesoura
+// sempreee asumiir que eu vou jogar tesoura
+// if 
+// else 
+// quem ganhou
+
+// console.log('wasdsasadas consoleeeeeee')
+// document.write(`<div>asdsadsa</div>`)
+
+
+
