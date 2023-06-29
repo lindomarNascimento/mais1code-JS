0b84d635d4051d2ebf2bef6a38219b7f0f684656
lindomar <linndomarnascimento17@gmail.com>
2023-06-29 20:26:33 -0300

    functions

--- index.js
+++ index.js
@@ -20,20 +20,20 @@
 // mapas || mapeamento 
 // DOM - Modelo de Objeto de Documentos 
 
-var input = document.querySelector('.input')
-var button = document.querySelector('.button')
-var result = document.querySelector('.result')
+// var input = document.querySelector('.input')
+// var button = document.querySelector('.button')
+// var result = document.querySelector('.result')
 
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-button.addEventListener('click', function () {
-  var number = Number(input.value) //1
-  // i = i+1  == i+=1 == i++
-  for (var i = 0; i < array.length; i++) {
-    result.innerHTML +=
-      `<li>${number}X ${array[i]} = ${number * array[i]}</li>`
-  }
-})
+// button.addEventListener('click', function () {
+//   var number = Number(input.value) //1
+//   // i = i+1  == i+=1 == i++
+//   for (var i = 0; i < array.length; i++) {
+//     result.innerHTML +=
+//       `<li>${number}X ${array[i]} = ${number * array[i]}</li>`
+//   }
+// })
 
 // index
 // mapas
@@ -57,5 +57,63 @@ button.addEventListener('click', function () {
 // 7 = 1, 2, 3, 4, 5, 6
 
 
+// console.log('aula de funções')
 
 
+// const nota1 = 1
+// // escopo {}
+// function teste() {
+//   const nota1 = 3
+// }
+// teste()
+
+
+// console.log('teste', nota1)
+
+
+
+// function helloWorld() {
+//   console.log('hello world')
+// }
+
+// function soma(numero1, numero2) {
+//   return numero1 + numero2
+// }
+
+// function selecionaDOM() {
+//   const input1 = document.querySelector('.value1')
+//   const input2 = document.querySelector('.value2')
+//   const button = document.querySelector('.button')
+
+//   button.addEventListener('click', function () {
+//     const valorDaSoma = soma(Number(input1.value), Number(input2.value))
+
+//     resultado(valorDaSoma)
+//     console.log('resultado', resultado)
+//   })
+// }
+
+// function resultado(resultadoSoma) {
+//   const resultado = document.querySelector('.result')
+
+//   resultado.innerHTML = `<h1>${resultadoSoma}</h1>`
+// }
+// selecionaDOM()
+
+
+//desafios
+
+// exercicio 4
+//  ajude o professor, ele digitara 3 notas em uma função , calcule a media em outra função e retorne 
+//  se ele passou ou não de ano  em uma terceira função
+// deixa BUNITO estiliza bastante. innerHTML e o input e functions, usar sem moderação
+
+// function funcao1() {} //guarda o valor das 3 notas digitadas
+// function funcao2() {} //calcula a media das 3 notas
+// function funcao3() {} //imprimi na tela se passou de ano
+
+// if ( todosEntregam ){
+//   faz calculadora
+// } else {
+//   acaba o curso
+// }
